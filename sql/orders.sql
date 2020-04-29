1a42fb4b79c5821d51a2062149f57200709b599d
Joshua Richard <jr681743@sju.edu>
2020-04-29 17:11:12 -0400

    final project

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,1 +1,1 @@
-Create table orders( oid Int primary key AUTO_INCREMENT, name VARCHAR(100), quantity int not null,  mid int UNIQUE, price decimal(6, 2) not null, id int not null, foreign key(id) REFERENCES users(id), foreign key(mid) REFERENCES menu(mid) )
+Create table orders( order_Id Int primary key AUTO_INCREMENT, name VARCHAR(100), quantity int not null,  mid int UNIQUE, price decimal(6, 2) not null, id int not null, foreign key(id) REFERENCES users(id), foreign key(mid) REFERENCES menu(mid) )
